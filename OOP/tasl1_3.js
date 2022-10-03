6fbcbd54379965aa020713b94d0bf080fa7f2f10
Gwan <slavik.art15@gmail.com>
2022-10-03 21:41:51 +0300

    completed fist task of OOP

---
+++ OOP/tasl1_3.js
@@ -0,0 +1,51 @@
+class Worker {
+    constructor(name, surname, rate, days) {
+        this._name = name;
+        this._surname = surname;
+        this._rate = rate;
+        this._days = days;
+    }
+    
+    setDays(quantity) {
+      this._days = quantity;
+    }
+
+    setRate(quan) {
+      this._rate = quan;
+    }
+
+    getName() {
+      return this._name;
+    }
+
+    getSurname() {
+      return this._surname;
+    }
+
+    getRate() {
+      return this._rate;
+    }
+
+    getDays() {
+      return this._days;
+    }
+
+    getSalary() {
+      return this._days * this._rate;
+    }
+
+}
+
+
+
+var worker = new Worker('Иван', 'Иванов', 10, 31);
+
+
+console.log(worker.getName()); //выведет 'Иван'
+console.log(worker.getSurname()); //выведет 'Иванов'
+console.log(worker.getRate()); //выведет 10
+console.log(worker.getDays()); //выведет 31
+
+worker.setRate(20); //увеличим ставку
+worker.setDays(10); //уменьшим дни
+console.log(worker.getSalary());
--- codewars/tasks/task5.js
+++ codewars/tasks/task5.js
@@ -1,9 +1,9 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 function even_or_odd(number) { 
       if (i % 2 == 0) {
-        return "Even"
+        return "Even";
       } else {
-        return "Odd"
+        return "Odd";
       }
   }
 
